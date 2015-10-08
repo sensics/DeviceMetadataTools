c9fd13950a468d8e6b6f82343217b74d2d0ea8e1
Ryan Pavlik <ryan@sensics.com>
2015-10-08 09:58:54 -0500

    Use glob-recurse and fix the program when you pass it file names

--- DeviceMetadataInstallTool/Program.cs
+++ DeviceMetadataInstallTool/Program.cs
@@ -4,32 +4,7 @@ using System.Collections;
 
 namespace DeviceMetadataInstallTool
 {
-    /// <summary>
-    /// see https://support.microsoft.com/en-us/kb/303974
-    /// </summary>
-    class MetadataFinder
-    {
-        public System.Collections.Generic.List<String> Files = new System.Collections.Generic.List<String>();
 
-        public void SearchDirectory(string dir)
-        {
-            try
-            {
-                foreach (var d in Directory.GetDirectories(dir))
-                {
-                    foreach (var f in Directory.GetFiles(d, "*.devicemetadata-ms"))
-                    {
-                        Files.Add(f);
-                    }
-                    SearchDirectory(d);
-                }
-            }
-            catch (System.Exception excpt)
-            {
-                Console.WriteLine(excpt.Message);
-            }
-        }
-    }
     class Program
     {
         static void Main(string[] args)
@@ -39,12 +14,11 @@ namespace DeviceMetadataInstallTool
             if (args.Length == 0)
             {
                 var assemblyDir = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-                var finder = new MetadataFinder();
-                finder.SearchDirectory(assemblyDir);
-                files = finder.Files;
-            } else
+                files = Sensics.DeviceMetadataInstaller.Util.GetMetadataFilesRecursive(assemblyDir);
+            }
+            else
             {
-                files.AddRange(files);
+                files.AddRange(args);
             }
 
             var store = new Sensics.DeviceMetadataInstaller.MetadataStore();
