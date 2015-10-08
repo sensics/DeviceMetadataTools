fd43e6cfaf9a4dcbff37aef864dd8dd02545573a
Ryan Pavlik <ryan@sensics.com>
2015-10-08 09:58:36 -0500

    Add glob-recurse functionality in the library

---
+++ Sensics.DeviceMetadataInstaller/GlobRecurse.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Sensics.DeviceMetadataInstaller
+{
+    /// <summary>
+    /// see https://support.microsoft.com/en-us/kb/303974
+    /// </summary>
+    internal class GlobRecurse
+    {
+        public static List<string> SearchDirectory(string dir, string pattern)
+        {
+            var obj = new GlobRecurse();
+            obj.DoSearchDirectory(dir, pattern);
+            return obj.Files;
+        }
+
+        private List<string> Files = new List<string>();
+
+        private void DoSearchDirectory(string dir, string pattern)
+        {
+            try
+            {
+                foreach (var d in Directory.GetDirectories(dir))
+                {
+                    foreach (var f in Directory.GetFiles(d, pattern))
+                    {
+                        Files.Add(f);
+                    }
+                    DoSearchDirectory(d, pattern);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+    }
+}
---
+++ Sensics.DeviceMetadataInstaller/Util.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Sensics.DeviceMetadataInstaller
+{
+
+    public class Util
+    {
+        public static List<string> GetMetadataFilesRecursive(string dir)
+        {
+            return GlobRecurse.SearchDirectory(dir, "*.devicemetadata-ms");
+        }
+    }
+}
