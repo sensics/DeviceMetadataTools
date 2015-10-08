a96fae262d335befd29e06490fca624ba84a25e4
Ryan Pavlik <ryan@sensics.com>
2015-10-08 10:02:42 -0500

    Functionality for the non-installing program

--- DeviceMetadataInspector/Program.cs
+++ DeviceMetadataInspector/Program.cs
@@ -1,15 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 
 namespace DeviceMetadataInspector
 {
     class Program
     {
+        static void HandleMetadata(IEnumerable<string> files)
+        {
+            foreach (var fn in files)
+            {
+                var pkg = new Sensics.DeviceMetadataInstaller.MetadataPackage(fn);
+                Console.WriteLine("{0} - {1} - Default locale: {2}", pkg.ExperienceGUID, pkg.ModelName, pkg.DefaultLocale);
+            }
+        }
         static void Main(string[] args)
         {
+            //Console.WriteLine("Args size is {0}", args.Length);
+            if (args.Length == 0)
+            {
+                var assemblyDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                HandleMetadata(Sensics.DeviceMetadataInstaller.Util.GetMetadataFilesRecursive(assemblyDir));
+            }
+            else
+            {
+                HandleMetadata(args);
+            }
         }
     }
 }
